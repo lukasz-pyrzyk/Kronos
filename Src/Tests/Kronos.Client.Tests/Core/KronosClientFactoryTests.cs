d227e33e85eeaa6c94f3d764a72e6f46effe27d2
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2015-12-22 16:26:57 +0100

    Missing tests

---
+++ Src/Tests/Kronos.Client.Tests/Core/KronosClientFactoryTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Kronos.Client.Tests.Core
+{
+    public class KronosClientFactoryTests
+    {
+        [Fact]
+        public void CanCreateIKronosClient()
+        {
+            IKronosClient client = KronosClientFactory.CreateClient();
+
+            Assert.NotNull(client);
+        }
+    }
+}
---
+++ Src/Tests/Kronos.Client.Tests/Core/KronosClientTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Kronos.Client.Core.Server;
+using Kronos.Shared.Network.Models;
+using NSubstitute;
+using Ploeh.AutoFixture;
+using Xunit;
+
+namespace Kronos.Client.Tests.Core
+{
+    public class KronosClientTests
+    {
+        private readonly Fixture _fixture = new Fixture();
+
+
+        [Fact]
+        public void CanSendRequestByKeyStreamAndExpiryDate()
+        {
+            string key = _fixture.Create<string>();
+            Stream stream = new MemoryStream(_fixture.Create<byte[]>());
+            DateTime expiryDate = _fixture.Create<DateTime>();
+            RequestStatusCode expectedStatusCode = RequestStatusCode.Ok;
+
+            IKronosCommunicationService communicationService = Substitute.For<IKronosCommunicationService>();
+            communicationService.SendToNode(Arg.Any<SocketRequest>()).Returns(expectedStatusCode);
+
+            IKronosClient client = new KronosClient(communicationService);
+            RequestStatusCode statusCode = client.SaveInCache(key, stream, expiryDate);
+
+            Assert.Equal(statusCode, expectedStatusCode);
+        }
+    }
+}
