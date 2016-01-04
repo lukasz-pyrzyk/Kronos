b2cedd6fbede1bfa378b0740f6c1f7a6f643d95a
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-01-04 23:57:04 +0100

    DNU -> dotnet command replace

--- Src/Prebuild.ps1
+++ Src/Prebuild.ps1
@@ -20,5 +20,5 @@ else
 # ex: & $env:USERPROFILE\.dnx\bin\dnvm install $dnxVersion -r coreclr
 & $env:USERPROFILE\.dnx\bin\dnvm install $dnxVersion -Persistent
 
- # run DNU restore on all project.json files in the src folder including 2>1 to redirect stderr to stdout for badly behaved tools
-Get-ChildItem -Path $PSScriptRoot\ -Filter project.json -Recurse | ForEach-Object { & dnu restore $_.FullName 2>1 }
+ # run dotne restore on all project.json files in the src folder including 2>1 to redirect stderr to stdout for badly behaved tools
+Get-ChildItem -Path $PSScriptRoot\ -Filter project.json -Recurse | ForEach-Object { & dotnet restore $_.FullName 2>1 }
