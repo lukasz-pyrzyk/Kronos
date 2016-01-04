267acb66b6a31d638ba8865f68c3422f2157d454
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-01-04 23:51:13 +0100

    Bugfix in prebuilds.ps1

--- Src/Prebuild.ps1
+++ Src/Prebuild.ps1
@@ -21,4 +21,4 @@ else
 & $env:USERPROFILE\.dnx\bin\dnvm install $dnxVersion -Persistent
 
  # run DNU restore on all project.json files in the src folder including 2>1 to redirect stderr to stdout for badly behaved tools
-Get-ChildItem -Path $PSScriptRoot\src -Filter project.json -Recurse | ForEach-Object { & dnu restore $_.FullName 2>1 }
+Get-ChildItem -Path $PSScriptRoot\ -Filter project.json -Recurse | ForEach-Object { & dnu restore $_.FullName 2>1 }
