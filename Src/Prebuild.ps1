24532d02e2539ba27a8e5d4ace0d4539eb18335d
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-01-05 21:34:19 +0100

    Another prebuild script

--- Src/Prebuild.ps1
+++ Src/Prebuild.ps1
@@ -1,9 +1,13 @@
-# bootstrap DNVM into this session.
+#Requires -Version 3.0
+ 
+param($vsoProjectName, $projectName, $buildConfiguration, $buildSourcesDirectory)
+ 
+$VerbosePreference = "continue"
+$ErrorActionPreference = "Stop"
+     
 &{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}
-
-# load up the global.json so we can find the DNX version
 $globalJson = Get-Content -Path $PSScriptRoot\global.json -Raw -ErrorAction Ignore | ConvertFrom-Json -ErrorAction Ignore
-
+ 
 if($globalJson)
 {
     $dnxVersion = $globalJson.sdk.version
@@ -13,12 +17,11 @@ else
     Write-Warning "Unable to locate global.json to determine using 'latest'"
     $dnxVersion = "latest"
 }
-
-# install DNX
-# only installs the default (x86, clr) runtime of the framework.
-# If you need additional architectures or runtimes you should add additional calls
-# ex: & $env:USERPROFILE\.dnx\bin\dnvm install $dnxVersion -r coreclr
+ 
 & $env:USERPROFILE\.dnx\bin\dnvm install $dnxVersion -Persistent
-
- # run dotnet restore command
-dotnet restore
+ 
+$dnxRuntimePath = "$($env:USERPROFILE)\.dnx\runtimes\dnx-clr-win-x86.$dnxVersion"
+     
+& "dnu" "build" "$PSScriptRoot\src\$projectName" "--configuration" "$buildConfiguration"
+ 
+& "dnu" "publish" "$PSScriptRoot\src\$projectName" "--configuration" "$buildConfiguration" "--out" "$buildSourcesDirectory\$vsoProjectName\artifacts\bin\$buildConfiguration\Publish" "--runtime" "$dnxRuntimePath"
