732fb320d9672e9a982af5708cb14f384f1e9919
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-01-09 14:46:15 +0100

    Deleted protobuf package

--- Src/Kronos.Shared/Network/Model/CachedObject.cs
+++ Src/Kronos.Shared/Network/Model/CachedObject.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Kronos.Shared.Network.Model
 {
-    [Serializable]
     public class CachedObject
     {
         public CachedObject(string key, byte[] objectToCache, DateTime expiryDate)
@@ -17,27 +14,5 @@ namespace Kronos.Shared.Network.Model
         public string Key { get; private set; }
         public byte[] Object { get; private set; }
         public DateTime ExpiryDate { get; private set; }
-
-        public byte[] SerializeNetworkPackage()
-        {
-            using (MemoryStream ms = new MemoryStream())
-            {
-                BinaryFormatter bf = new BinaryFormatter();
-                bf.Serialize(ms, this);
-                return ms.ToArray();
-            }
-        }
-
-        public static CachedObject DeserializeNetworkPackage(byte[] package)
-        {
-            using (MemoryStream memStream = new MemoryStream())
-            {
-                BinaryFormatter binForm = new BinaryFormatter();
-                memStream.Write(package, 0, package.Length);
-                memStream.Seek(0, SeekOrigin.Begin);
-                CachedObject obj = (CachedObject)binForm.Deserialize(memStream);
-                return obj;
-            }
-        }
     }
 }
