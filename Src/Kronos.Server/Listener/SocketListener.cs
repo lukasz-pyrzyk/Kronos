4257b1ff26aad0a212380044915a790b0e05f55f
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-01-25 21:33:25 +0100

    Reading request to byte array and deserialization

--- Src/Kronos.Server/Listener/SocketListener.cs
+++ Src/Kronos.Server/Listener/SocketListener.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using Kronos.Core.Requests;
 using NLog;
 
 namespace Kronos.Server.Listener
@@ -33,33 +35,43 @@ namespace Kronos.Server.Listener
                     {
                         connectionRequest = server.Accept();
                         var timer = Stopwatch.StartNew();
-                        _logger.Info("Accepting request");
+                        _logger.Info("Accepting new request");
 
                         byte[] packageSizeBuffer = new byte[sizeof (int)];
                         _logger.Info("Receiving information about request size");
                         connectionRequest.Receive(packageSizeBuffer, SocketFlags.None);
-                        int requestSize = BitConverter.ToInt32(packageSizeBuffer, 0);
+
+                        int requestSize = BitConverter.ToInt32(packageSizeBuffer, 0) + sizeof(int);
                         _logger.Info($"Request contains {requestSize} bytes");
 
-                        int readedBytes = sizeof (int);
-                        while (readedBytes != requestSize)
+                        byte[] requestPackage = new byte[requestSize];
+                        Buffer.BlockCopy(packageSizeBuffer, 0, requestPackage, 0, packageSizeBuffer.Length);
+                        int offset = packageSizeBuffer.Length;
+                        while (offset != requestSize)
                         {
                             byte[] package = new byte[packageSize];
 
                             _logger.Info($"Receiving {packageSize} bytes");
                             int received = connectionRequest.Receive(package, SocketFlags.None);
-                            readedBytes += received;
-                            _logger.Info($"Total received bytes: {(float) readedBytes*100/requestSize}%");
+                            if (package.Length != received)
+                            {
+                                package = package.Take(received).ToArray();
+                            }
+                            Buffer.BlockCopy(package, 0, requestPackage, offset, package.Length);
+                            offset += received;
+                            _logger.Info($"Total received bytes: {(float) offset*100/requestSize}%");
                         }
 
                         timer.Stop();
                         _logger.Info($"Finished receiving package in {timer.ElapsedMilliseconds}ms");
 
                         _logger.Info("Sending response to the client");
-                        connectionRequest.Send(BitConverter.GetBytes(readedBytes));
+                        connectionRequest.Send(BitConverter.GetBytes(offset));
 
                         _logger.Info("Disposing request");
                         connectionRequest.Dispose();
+
+                        InsertRequest request = InsertRequest.Deserialize(requestPackage);
                     }
                     catch (SocketException ex)
                     {
