b4c8bd2f533ddcf625aae4f987e3e24b78838ed5
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-02-07 13:43:55 +0100

    NetworkStream instead of own transfer logic

---
+++ Src/Tests/Kronos.Server.Tests/RequestProcessing/RequestProcessorTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using Kronos.Server.RequestProcessing;
+using Moq;
+using Xunit;
+
+namespace Kronos.Server.Tests.RequestProcessing
+{
+    public class RequestProcessorTests
+    {
+        [Fact]
+        public void ThrowsAnExceptionWhenRequestTypeWillBeIncorrect()
+        {
+            RequestProcessor processor = new RequestProcessor();
+            byte[] bytes = Encoding.UTF8.GetBytes("lorem ipsum");
+
+            Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+
+            Assert.ThrowsAny<InvalidOperationException>(() => processor.ProcessRequest(socket, bytes));
+        }
+    }
+}
