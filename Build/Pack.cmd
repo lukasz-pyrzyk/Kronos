9d21fc513bc7d55e656d649b14ad369b45dfd7be
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-02-15 21:02:27 +0100

    Build packages without coping to the special output folder

--- Build/Pack.cmd
+++ Build/Pack.cmd
@@ -2,11 +2,11 @@ set version-suffix=%1
 
 cd ..\Src\
 cd Kronos.Core
-dotnet pack --configuration --Release --version-suffix %version-suffix% -o ..\..\Build\Packages
+dotnet pack --configuration --Release --version-suffix %version-suffix%
 cd ..
 cd Kronos.Client
-dotnet pack --configuration --Release --version-suffix %version-suffix% -o ..\..\Build\Packages
+dotnet pack --configuration --Release --version-suffix %version-suffix%
 cd ..
 cd Kronos.Server
-dotnet pack --configuration --Release --version-suffix %version-suffix% -o ..\..\Build\Packages
+dotnet pack --configuration --Release --version-suffix %version-suffix%
 exit
