2ba1ba59c02d7c3b9f64c396f353895e67d33e09
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-02-24 20:39:34 +0100

    KronosException and KronosCommunicationException

---
+++ Src/Kronos.Core/Model/Exceptions/KronosCommunicationException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Kronos.Core.Model.Exceptions
+{
+    public class KronosCommunicationException : KronosException
+    {
+        public KronosCommunicationException()
+        {
+        }
+
+        public KronosCommunicationException(string message)
+            : base(message)
+        {
+        }
+
+        public KronosCommunicationException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
