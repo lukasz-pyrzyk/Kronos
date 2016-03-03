f46180409806d6a3b31aa38ac6b6e385e69fcf90
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-03-03 17:07:27 +0100

    Unit tests for exceptions

---
+++ Src/Tests/Kronos.Core.Tests/Exceptions/KronosExceptionTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Kronos.Core.Model.Exceptions;
+using Xunit;
+
+namespace Kronos.Core.Tests.Exceptions
+{
+    public class KronosExceptionTests
+    {
+        [Fact]
+        public void Ctor_CanAssignMessage()
+        {
+            string messsage = "lorem ipsum";
+            KronosException ex = new KronosException(messsage);
+
+            Assert.Equal(ex.Message, messsage);
+        }
+
+        [Fact]
+        public void Ctor_CanAssignMessageAndInnerException()
+        {
+            string messsage = "lorem ipsum";
+            Exception innerException = new Exception();
+            KronosException ex = new KronosException(messsage, innerException);
+
+            Assert.Equal(ex.Message, messsage);
+            Assert.Equal(ex.InnerException, innerException);
+        }
+    }
+}
