9d947dba30d87c6cb0ee9419a8a4f5ce6a702950
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-03-06 09:58:37 +0100

    Added unit tests for commands

--- Src/Tests/Kronos.Core.Tests/Command/CommandTests.cs
+++ Src/Tests/Kronos.Core.Tests/Command/CommandTests.cs
@@ -1,6 +1,8 @@
-﻿using Kronos.Core.Command;
+﻿using System.Text;
+using Kronos.Core.Command;
 using Kronos.Core.Communication;
 using Kronos.Core.Storage;
+using Moq;
 using Xunit;
 
 namespace Kronos.Core.Tests.Command
@@ -15,10 +17,27 @@ namespace Kronos.Core.Tests.Command
             Assert.NotNull(command);
         }
 
+        [Fact]
+        public void SendToClient_SendsBufferToClient()
+        {
+            var socketMock = new Mock<ISocket>();
+            byte[] buffer = Encoding.UTF8.GetBytes("lorem ipsum");
+
+            FakeCommand command = new FakeCommand();
+            command.SendToClientFake(socketMock.Object, buffer);
+
+            socketMock.Verify(x => x.Send(buffer), Times.Exactly(1));
+        }
+
         internal class FakeCommand : BaseCommand
         {
             public override void ProcessRequest(ISocket socket, byte[] requestBytes, IStorage storage)
             {
+            }
+
+            public void SendToClientFake(ISocket clientSocket, byte[] buffer)
+            {
+                clientSocket.Send(buffer);
             }
         }
     }
--- Src/Tests/Kronos.Core.Tests/Command/InsertCommandTests .cs
+++ Src/Tests/Kronos.Core.Tests/Command/InsertCommandTests .cs
@@ -1,8 +1,10 @@
-﻿using Kronos.Core.Command;
+﻿using System;
+using Kronos.Core.Command;
 using Kronos.Core.Communication;
 using Kronos.Core.Requests;
 using Kronos.Core.Serialization;
 using Kronos.Core.StatusCodes;
+using Kronos.Core.Storage;
 using Moq;
 using Xunit;
 
@@ -25,6 +27,25 @@ namespace Kronos.Core.Tests.Command
 
             Assert.Equal(response, status);
             communicationServiceMock.Verify(x => x.SendToServer(It.IsAny<InsertRequest>()), Times.Exactly(1));
+        }
+
+        [Fact]
+        public void ProcessRequest_AddsObjectToStorage()
+        {
+            string key = "lorem ipsum";
+            byte[] cachedObject = SerializationUtils.Serialize("object");
+            DateTime expiryDate = DateTime.Today;
+
+            var storageMock = new Mock<IStorage>();
+            var socketMock = new Mock<ISocket>();
+
+            InsertCommand command = new InsertCommand();
+            byte[] requtestBytes = SerializationUtils.Serialize(new InsertRequest(key, cachedObject, expiryDate));
+            command.ProcessRequest(socketMock.Object, requtestBytes, storageMock.Object);
+
+            storageMock.Verify(x => x.AddOrUpdate(key, cachedObject), Times.Once);
+            byte[] responseBytes = SerializationUtils.Serialize(RequestStatusCode.Ok);
+            socketMock.Verify(x => x.Send(responseBytes), Times.Once);
         }
     }
 }
