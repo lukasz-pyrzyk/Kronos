82aec7855db42e440dedb2714ffa054b2495f85d
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-03-06 10:11:17 +0100

    Unit test for RequestType property

--- Src/Tests/Kronos.Core.Tests/Requests/RequestTests.cs
+++ Src/Tests/Kronos.Core.Tests/Requests/RequestTests.cs
@@ -1,6 +1,17 @@
-﻿namespace Kronos.Core.Tests.Requests
+﻿using Kronos.Core.Requests;
+using Xunit;
+
+namespace Kronos.Core.Tests.Requests
 {
     public class RequestTests
     {
+        [Fact]
+        public void Ctor_CanAssignRequestType()
+        {
+            RequestType type = RequestType.GetRequest;
+            Request r = new Request { RequestType = type };
+
+            Assert.Equal(type, r.RequestType);
+        }
     }
 }
