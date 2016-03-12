2f630807f7573d1da4308397e249f311b3f066a0
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-03-12 15:54:28 +0100

    Loop of 10000 iterations

--- Sample/ClientSample/Program.cs
+++ Sample/ClientSample/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Globalization;
 using System.Net;
 using System.Text;
 using Kronos.Client;
+using Kronos.Core.Serialization;
 
 namespace ClientSample
 {
@@ -9,20 +12,26 @@ namespace ClientSample
     {
         public static void Main(string[] args)
         {
-            string host = "23.101.135.101"; // IP of the Kronos.Server node
-            int port = 5001;  // Opened port in the node
+            string host = "192.168.56.1"; // IP of the Kronos.Server node
+            int port = 5000;  // Opened port in the node
 
             IKronosClient client = KronosClientFactory.CreateClient(IPAddress.Parse(host), port);
 
-            string key = Guid.NewGuid().ToString();
-            byte[] package = Encoding.UTF8.GetBytes("lorem ipsum");
+            string key = "key";
+            byte[] package = Encoding.UTF8.GetBytes("elo");
             DateTime expiryDate = DateTime.Now.AddDays(1);
 
             client.InsertToServer(key, package, expiryDate);
 
-            byte[] fromServer = client.TryGetValue(key);
+            var watch = Stopwatch.StartNew();
+            for (int i = 0; i < 10000; i++)
+            {
+                byte[] fromServer = client.TryGetValue(key);
+                Console.WriteLine($"{SerializationUtils.Deserialize<string>(fromServer)} - {DateTime.Now.ToString(CultureInfo.InvariantCulture)}");
+            }
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds);
 
-            Console.WriteLine(Encoding.UTF8.GetString(fromServer));
             Console.ReadKey();
         }
     }
