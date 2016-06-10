000e552737e1d5ead117e2bbce905b283113be34
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-06-10 17:13:27 +0200

    Added Benchmark project

---
+++ Src/Kronos.Benchmark/Program.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using System;
+
+namespace Kronos.Benchmark
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmark_StoragePerformance>(DefaultConfig.Instance.RemoveBenchmarkFiles());
+        }
+    }
+
+    public class Benchmark_StoragePerformance
+    {
+    }
+}
