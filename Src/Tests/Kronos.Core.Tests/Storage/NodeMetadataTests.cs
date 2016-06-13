18461f220532ecd3a6b3f5fed4a1d4461b240d93
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-06-13 21:53:00 +0200

    Added comparer for NodeMetadata

---
+++ Src/Tests/Kronos.Core.Tests/Storage/NodeMetadataTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Kronos.Core.Storage;
+using Xunit;
+
+namespace Kronos.Core.Tests.Storage
+{
+    public class NodeMetadataTests
+    {
+        [Theory]
+        [MemberData(nameof(ArgumentsData))]
+        public void Ctor_AssignsProperties(string key, DateTime expiryDate)
+        {
+            var metadata = new NodeMetatada(key, expiryDate);
+
+            Assert.Equal(key, metadata.Key);
+            Assert.Equal(expiryDate, metadata.ExpiryDate);
+        }
+
+        [Fact]
+        public void ToString_ContainsInformationAboutKeyAndExpiry()
+        {
+            string key = "key";
+            DateTime expiryDate = DateTime.Now;
+            var metadata = new NodeMetatada(key, expiryDate);
+            string message = metadata.ToString();
+
+            Assert.Equal($"{key}|{expiryDate:o}", message);
+        }
+
+        [Fact]
+        public void HashCode_ReturnsHashCodeFromKey()
+        {
+            string key = "key";
+            var metadata = new NodeMetatada(key);
+
+            Assert.Equal(key.GetHashCode(), metadata.GetHashCode());
+        }
+
+
+        public static IEnumerable<object[]> ArgumentsData => new[]
+        {
+            new object[] { "key", DateTime.Now },
+            new object[] { "key", null }
+        };
+    }
+}
