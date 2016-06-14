2c7822c1af337d7fb3ee05a97f06d97610a2c2a4
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-06-14 22:42:51 +0200

    Usage of NodeMetaData and expiryProvider

--- Src/Kronos.Core/Storage/InMemoryStorage.cs
+++ Src/Kronos.Core/Storage/InMemoryStorage.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Concurrent;
+using System.Threading;
 using NLog;
 
 namespace Kronos.Core.Storage
@@ -6,21 +7,33 @@ namespace Kronos.Core.Storage
     public class InMemoryStorage : IStorage
     {
         private readonly Logger _logger = LogManager.GetCurrentClassLogger();
-        private readonly ConcurrentDictionary<string, byte[]> _storage = new ConcurrentDictionary<string, byte[]>();
+
+        private readonly ConcurrentDictionary<NodeMetatada, byte[]> _storage =
+            new ConcurrentDictionary<NodeMetatada, byte[]>(new NodeComparer());
+
+        private readonly CancellationTokenSource _cancel = new CancellationTokenSource();
 
         public int Count => _storage.Count;
+
+        public InMemoryStorage()
+        {
+            var provider = new StorageExpiryProvider();
+            provider.StartWork(_storage, _cancel);
+        }
 
         public void AddOrUpdate(string key, byte[] obj)
         {
             _logger.Debug($"Inserting key {key} to MemoryStorage");
-            _storage.AddOrUpdate(key, obj, (s, bytes) => obj);
+            var metaData = new NodeMetatada(key);
+            _storage.AddOrUpdate(metaData, obj, (s, bytes) => obj);
         }
 
         public byte[] TryGet(string key)
         {
             _logger.Debug($"Getting package for key {key}");
+            var metaData = new NodeMetatada(key);
             byte[] obj;
-            if (_storage.TryGetValue(key, out obj))
+            if (_storage.TryGetValue(metaData, out obj))
             {
                 _logger.Debug($"Returning object of key {key}");
                 return obj;
@@ -33,12 +46,15 @@ namespace Kronos.Core.Storage
         public bool TryRemove(string key)
         {
             byte[] obj;
-            return _storage.TryRemove(key, out obj);
+            var metaData = new NodeMetatada(key);
+            return _storage.TryRemove(metaData, out obj);
         }
 
         public void Dispose()
         {
             _logger.Info("Disposing storage");
+
+            _cancel.Cancel();
             _storage.Clear();
         }
     }
