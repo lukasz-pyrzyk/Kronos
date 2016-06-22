cd7220a1e925af00801479ba7a130487ef23dc7d
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-06-22 18:29:11 +0200

    Script for running acceptance tests

---
+++ Scripts/runAcceptanceTests.ps1
@@ -0,0 +1,29 @@
+$projects = @(
+    "Src\Tests\Kronos.AcceptanceTest\"
+)
+
+# test function for project
+function RunTests($path) {
+    dotnet test $path
+}
+
+function RestorePackages(){
+    dotnet restore
+}
+
+write-host "Acceptance tests started"
+
+# restore packages
+write-host "Restoring packages"
+RestorePackages
+
+# run tests
+foreach ($project in $projects){
+    write-host "Testing " $project
+    RunTests($project)
+}
+
+# Set build as failed if any error occurred
+if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
+
+write-host "Acceptance tests finished"
