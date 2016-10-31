565c5c4ba219fc2effe3d80c9694c3b55496cd3a
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-10-31 18:32:33 +0100

    Backing field for IPEndpoint

--- Src/Kronos.Core/Configuration/ServerConfig.cs
+++ Src/Kronos.Core/Configuration/ServerConfig.cs
@@ -6,13 +6,15 @@ namespace Kronos.Core.Configuration
     [DataContract]
     public class ServerConfig
     {
+        private IPEndPoint _endPoint;
+
         [DataMember]
         public string Ip { get; set; }
 
         [DataMember]
         public int Port { get; set; }
 
-        public IPEndPoint EndPoint  => new IPEndPoint(IPAddress.Parse(Ip), Port);
+        public IPEndPoint EndPoint => _endPoint ?? (_endPoint = new IPEndPoint(IPAddress.Parse(Ip), Port));
 
         public override string ToString()
         {
