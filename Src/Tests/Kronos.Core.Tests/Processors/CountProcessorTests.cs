d242cfa57fbcb9997482392aa54a2e154994760a
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-11-01 12:58:24 +0100

    Tests for count

---
+++ Src/Tests/Kronos.Core.Tests/Processors/CountProcessorTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Net.Sockets;
+using Kronos.Core.Communication;
+using Kronos.Core.Processors;
+using Kronos.Core.Requests;
+using Kronos.Core.Serialization;
+using Kronos.Core.Storage;
+using NSubstitute;
+using XGain.Sockets;
+using Xunit;
+
+namespace Kronos.Core.Tests.Processors
+{
+    public class CountProcessorTests
+    {
+        [Fact]
+        public void Handle_ReturnsNumberOfElementInStorage()
+        {
+            // arrange
+            bool expected = true;
+            var request = new CountRequest();
+            var processor = new CountProcessor();
+            int count = 5;
+            var storage = Substitute.For<IStorage>();
+            storage.Count.Returns(count);
+
+            byte[] expectedBytes = SerializationUtils.SerializeToStreamWithLength(count);
+            var communicationServiceMock = Substitute.For<IClientServerConnection>();
+            communicationServiceMock
+                .Send(request)
+                .Returns(SerializationUtils.SerializeToStreamWithLength(expected));
+            var socket = Substitute.For<ISocket>();
+            socket.Send(Arg.Any<byte[]>(), Arg.Any<int>(), Arg.Any<int>(), Arg.Any<SocketFlags>())
+                .Returns(expectedBytes.Length);
+
+            // act
+            processor.Handle(ref request, storage, socket);
+
+            // assert
+            socket.Received(1).Send(Arg.Is<byte[]>(x => x.SequenceEqual(expectedBytes)), Arg.Any<int>(), Arg.Any<int>(), SocketFlags.None);
+        }
+    }
+}
