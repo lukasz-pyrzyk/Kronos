d915500d98f0de6649dad4419e48d87ca6e4b6a1
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-11-18 18:36:14 +0100

    netcoreapp1.0 as a runtime for tests

--- Scripts/Linux/runTests.sh
+++ Scripts/Linux/runTests.sh
@@ -1,6 +1,7 @@
 dotnet restore
 
-dotnet test Src/Kronos.Core/project.json -f netstandard1.3
-dotnet test Src/Kronos.Client/project.json -f netstandard1.3
-dotnet test Src/Kronos.Server/project.json -f netstandard1.3
-dotnet test Src/Kronos.AcceptanceTest/project.json -f netstandard1.3
+RUNTIME="netcoreapp1.0"
+dotnet test Src/Kronos.Core/project.json -f $RUNTIME
+dotnet test Src/Kronos.Client/project.json -f $RUNTIME
+dotnet test Src/Kronos.Server/project.json -f $RUNTIME
+dotnet test Src/Kronos.AcceptanceTest/project.json -f $RUNTIME
