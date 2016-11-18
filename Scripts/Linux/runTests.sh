38a0352bbe5005719c4efa125950b2039b0f0097
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-11-18 17:48:00 +0100

    Tests in -f netstandard1.3 mode

--- Scripts/Linux/runTests.sh
+++ Scripts/Linux/runTests.sh
@@ -1,6 +1,6 @@
 dotnet restore
 
-dotnet test Src/Kronos.Core/project.json
-dotnet test Src/Kronos.Client/project.json
-dotnet test Src/Kronos.Server/project.json
-dotnet test Src/Kronos.AcceptanceTest/project.json
+dotnet test Src/Kronos.Core/project.json -f netstandard1.3
+dotnet test Src/Kronos.Client/project.json -f netstandard1.3
+dotnet test Src/Kronos.Server/project.json -f netstandard1.3
+dotnet test Src/Kronos.AcceptanceTest/project.json -f netstandard1.3
