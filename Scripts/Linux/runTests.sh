166b86ae73218dfd72e438e7ae6c3144bd433879
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-11-18 17:42:58 +0100

    Added acceptanceTest

--- Scripts/Linux/runTests.sh
+++ Scripts/Linux/runTests.sh
@@ -3,3 +3,4 @@ dotnet restore
 dotnet test Src/Kronos.Core/project.json
 dotnet test Src/Kronos.Client/project.json
 dotnet test Src/Kronos.Server/project.json
+dotnet test Src/Kronos.AcceptanceTest/project.json
