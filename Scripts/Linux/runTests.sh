9566df7cae7afecb0a1a00c496c225dd91beff0f
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-11-18 18:45:39 +0100

    Fixed bad path to the test folder

--- Scripts/Linux/runTests.sh
+++ Scripts/Linux/runTests.sh
@@ -1,7 +1,7 @@
 dotnet restore
 
 RUNTIME="netcoreapp1.0"
-dotnet test Src/Kronos.Core/project.json -f $RUNTIME
-dotnet test Src/Kronos.Client/project.json -f $RUNTIME
-dotnet test Src/Kronos.Server/project.json -f $RUNTIME
-dotnet test Src/Kronos.AcceptanceTest/project.json -f $RUNTIME
+dotnet test Tests/Kronos.Core/project.json -f $RUNTIME
+dotnet test Tests/Kronos.Client/project.json -f $RUNTIME
+dotnet test Tests/Kronos.Server/project.json -f $RUNTIME
+dotnet test Tests/Kronos.AcceptanceTest/project.json -f $RUNTIME
