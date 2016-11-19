08178532d967234b7d0b6355822cad4cc275f28e
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-11-19 10:55:52 +0100

    Removed manual GC call

--- Src/Kronos.Core/Storage/StorageExpiryProvider.cs
+++ Src/Kronos.Core/Storage/StorageExpiryProvider.cs
@@ -10,7 +10,7 @@ namespace Kronos.Core.Storage
     public class StorageExpiryProvider : IExpiryProvider
     {
         public static readonly int Timer = 1000;
-        private int _runsWithoutGc;
+
         private static readonly ILogger _logger = LogManager.GetCurrentClassLogger();
 
         public void Start(ConcurrentDictionary<NodeMetatada, byte[]> nodes, CancellationToken token)
@@ -36,28 +36,10 @@ namespace Kronos.Core.Storage
                         _logger.Info($"Deleted {deleted} elements from storage");
                     }
 
-                    _runsWithoutGc++;
-                    if (_runsWithoutGc >= 25)
-                    {
-                        _runsWithoutGc = 0;
-                        FlushMemory();
-                    }
-
                     await Task.Delay(Timer, token);
                 }
 
             }, TaskCreationOptions.LongRunning);
-        }
-
-        private void FlushMemory()
-        {
-            long memory = GC.GetTotalMemory(true);
-            _logger.Debug($"Memory was flushed. Current usage: {ConvertBytesToMegabytes(memory)}mb");
-        }
-
-        private static string ConvertBytesToMegabytes(long bytes)
-        {
-            return (bytes / 1024f / 1024f).ToString("0.00");
         }
     }
 }
