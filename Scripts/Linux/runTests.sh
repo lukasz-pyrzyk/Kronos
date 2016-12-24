9567d3b92017ed8949224e3303fa701bc8d9b550
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-12-24 17:19:07 +0100

    Removed call to project.json

--- Scripts/Linux/runTests.sh
+++ Scripts/Linux/runTests.sh
@@ -4,7 +4,7 @@ set -e
 dotnet restore
 
 RUNTIME="netcoreapp1.1"
-dotnet test Tests/Kronos.Core.Tests/project.json -f $RUNTIME
-dotnet test Tests/Kronos.Client.Tests/project.json -f $RUNTIME
-dotnet test Tests/Kronos.Server.Tests/project.json -f $RUNTIME
-dotnet test Tests/Kronos.AcceptanceTest/project.json -f $RUNTIME
+dotnet test Tests/Kronos.Core.Tests/ -f $RUNTIME
+dotnet test Tests/Kronos.Client.Tests/ -f $RUNTIME
+dotnet test Tests/Kronos.Server.Tests/ -f $RUNTIME
+dotnet test Tests/Kronos.AcceptanceTest/ -f $RUNTIME
