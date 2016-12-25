5607707455b2cc2edfe59bbc2f2b08043cebdc50
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-12-25 20:30:17 +0100

    Removed suffix version from build

--- Scripts/Windows/build.ps1
+++ Scripts/Windows/build.ps1
@@ -9,7 +9,7 @@ $projects = @(
 
 # build function for project
 function Build($path) {
-    dotnet build $path -c Release --version-suffix $version --no-incremental
+    dotnet build $path -c Release
 }
 
 function RestorePackages(){
