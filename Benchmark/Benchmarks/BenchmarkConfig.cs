dab8d90123e1c85a49a8c7a729f86dc602845bdd
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-01-10 13:33:03 +0100

    Added custom config

---
+++ Benchmark/Benchmarks/BenchmarkConfig.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+
+namespace ClusterBenchmark.Benchmarks
+{
+    public class BenchmarkConfig : ManualConfig
+    {
+        public BenchmarkConfig()
+        {
+            Add(StatisticColumn.Min);
+            Add(StatisticColumn.Max);
+            Add(MemoryDiagnoser.Default);
+            Add(MarkdownExporter.GitHub);
+            Add(PlainExporter.Default);
+            Add(AsciiDocExporter.Default);
+        }
+    }
+}
