fc1bca39e5866f44320d8db1d11cd2cefde6df71
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-01-10 13:46:19 +0100

    Added order provider

--- Benchmark/Benchmarks/BenchmarkConfig.cs
+++ Benchmark/Benchmarks/BenchmarkConfig.cs
@@ -1,11 +1,13 @@
-﻿using BenchmarkDotNet.Columns;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Exporters;
-using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Order;
 
 namespace ClusterBenchmark.Benchmarks
 {
+    [OrderProvider(SummaryOrderPolicy.FastestToSlowest)]
     public class BenchmarkConfig : ManualConfig
     {
         public BenchmarkConfig()
