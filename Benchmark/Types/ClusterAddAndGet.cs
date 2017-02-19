f4ef1ffc223ce758f7de461affe6c5b30b98db8a
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-02-19 12:42:55 +0100

    Added cluster add and get benchmark

---
+++ Benchmark/Types/ClusterAddAndGet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Benchmark.Config;
+using Benchmark.Utils;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmark.Types
+{
+    public class ClusterAddAndGet : ClusterBenchmark
+    {
+        [Params(Size.Mb)]
+        public int PackageSize { get; set; }
+
+        private byte[] _data;
+
+        protected override void AdditionalSetup()
+        {
+            _data = Prepare.Bytes(PackageSize);
+
+            Task kronos = KronosClient.ClearAsync();
+            Task[] redisTasks = RedisServers.Select(x => x.FlushAllDatabasesAsync()).ToArray();
+            Task.WaitAll(redisTasks);
+            Task.WaitAll(kronos);
+        }
+
+        [Benchmark]
+        public async Task<byte[]> Kronos()
+        {
+            string key = Prepare.Key();
+            await KronosClient.InsertAsync(key, _data, DateTime.UtcNow.AddMinutes(5))
+                .ConfigureAwait(false);
+
+            return await KronosClient.GetAsync(key)
+                .ConfigureAwait(false);
+        }
+
+        [Benchmark]
+        public async Task<byte[]> Redis()
+        {
+            string key = Prepare.Key();
+            await RedisClient.SetAddAsync(key, _data)
+                .ConfigureAwait(false);
+
+            return await RedisClient.StringGetAsync(key);
+        }
+    }
+}
