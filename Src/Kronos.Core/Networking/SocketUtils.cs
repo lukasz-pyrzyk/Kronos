7be6bb7043148f3fb53714e8a9c59fa3fc7e0bf0
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-02-19 10:30:40 +0100

    One async task for send/receive

--- Src/Kronos.Core/Networking/SocketUtils.cs
+++ Src/Kronos.Core/Networking/SocketUtils.cs
@@ -11,10 +11,15 @@ namespace Kronos.Core.Networking
 
         public static void SendAll(Socket socket, byte[] data)
         {
+            SendAll(socket, data, data.Length);
+        }
+
+        public static void SendAll(Socket socket, byte[] data, int count)
+        {
             int position = 0;
-            while (position != data.Length)
+            while (position != count)
             {
-                int size = Math.Min(data.Length - position, BufferSize);
+                int size = Math.Min(count - position, BufferSize);
                 int sent = socket.Send(data, position, size, SocketFlags.None);
                 position += sent;
 
@@ -22,17 +27,22 @@ namespace Kronos.Core.Networking
             }
         }
 
-        public static async Task SendAllAsync(Socket socket, byte[] data, int count)
-        {
-            int position = 0;
-            while (position != count)
-            {
-                int size = Math.Min(count - position, BufferSize);
-                int sent = await socket.SendAsync(new ArraySegment<byte>(data, position, size), SocketFlags.None).ConfigureAwait(false);
-                position += sent;
+        //public static async Task SendAllAsync(Socket socket, byte[] data, int count)
+        //{
+        //    int position = 0;
+        //    while (position != count)
+        //    {
+        //        int size = Math.Min(count - position, BufferSize);
+        //        int sent = await socket.SendAsync(new ArraySegment<byte>(data, position, size), SocketFlags.None).ConfigureAwait(false);
+        //        position += sent;
 
-                Debug.Assert(position <= data.Length);
-            }
+        //        Debug.Assert(position <= data.Length);
+        //    }
+        //}
+
+        public static Task SendAllAsync(Socket socket, byte[] data, int count)
+        {
+            return Task.Run(() => SendAll(socket, data));
         }
 
         public static void ReceiveAll(Socket socket, byte[] data, int count)
@@ -48,18 +58,23 @@ namespace Kronos.Core.Networking
             }
         }
 
-        public static async Task ReceiveAllAsync(Socket socket, byte[] data, int count)
+        public static Task ReceiveAllAsync(Socket socket, byte[] data, int count)
         {
-            int position = 0;
-            while (position != count)
-            {
-                int size = Math.Min(count - position, BufferSize);
-                var segment = new ArraySegment<byte>(data, position, size);
-                int received = await socket.ReceiveAsync(segment, SocketFlags.None).ConfigureAwait(false);
-                position += received;
-
-                Debug.Assert(position <= count);
-            }
+            return Task.Run(() => ReceiveAll(socket, data, count));
         }
+
+        //public static async Task ReceiveAllAsync(Socket socket, byte[] data, int count)
+        //{
+        //    int position = 0;
+        //    while (position != count)
+        //    {
+        //        int size = Math.Min(count - position, BufferSize);
+        //        var segment = new ArraySegment<byte>(data, position, size);
+        //        int received = await socket.ReceiveAsync(segment, SocketFlags.None).ConfigureAwait(false);
+        //        position += received;
+
+        //        Debug.Assert(position <= count);
+        //    }
+        //}
     }
 }
