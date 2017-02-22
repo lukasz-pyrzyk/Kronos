74d6f8885536e4033fbbd774ca5bb2e84c6c55ac
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-02-22 18:27:23 +0100

    Improved logging on the automated test task

--- Tests/Kronos.AcceptanceTest/Base.cs
+++ Tests/Kronos.AcceptanceTest/Base.cs
@@ -4,20 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Kronos.Client;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace Kronos.AcceptanceTest
 {
     [Collection("AcceptanceTest")]
     public abstract class Base
     {
-        protected readonly ITestOutputHelper output;
-
-        protected Base(ITestOutputHelper output)
-        {
-            this.output = output;
-        }
-
         private static readonly ConcurrentQueue<int> ports = new ConcurrentQueue<int>(Enumerable.Range(5000, 6000));
 
         [Fact]
@@ -29,44 +21,49 @@ namespace Kronos.AcceptanceTest
                 throw new Exception("Available ports queue is empty");
             }
 
-            output.WriteLine($"Creating client with port {port}");
+            LogMessage($"Creating kronos client with port {port}");
             IKronosClient client = KronosClientFactory.FromLocalhost(port);
 
-            output.WriteLine($"Creating server with port {port}");
+            LogMessage($"Creating server with port {port}");
             Task server = Server.Program.StartAsync(port);
 
-            output.WriteLine($"Waiting for server warnup");
+            LogMessage($"Waiting for server warnup");
             await Task.Delay(2000);
 
             try
             {
-                output.WriteLine("Processing internal test");
+                LogMessage("Processing internal test");
                 await ProcessAsync(client).AwaitWithTimeout(5000);
-                output.WriteLine("Processing internal finished");
+                LogMessage("Processing internal finished");
             }
             catch (Exception ex)
             {
-                output.WriteLine($"EXCEPTION: {ex}");
+                LogMessage($"EXCEPTION: {ex}");
                 Assert.False(true, ex.Message);
             }
             finally
             {
                 try
                 {
-                    output.WriteLine("Stopping server");
+                    LogMessage("Stopping server");
                     Server.Program.Stop();
 
-                    output.WriteLine("Waiting for server task to finish");
+                    LogMessage("Waiting for server task to finish");
                     await server.AwaitWithTimeout(5000);
 
-                    output.WriteLine("Server stopped");
+                    LogMessage("Server stopped");
                 }
                 catch (Exception ex)
                 {
-                    output.WriteLine($"EXCEPTION: {ex}");
+                    LogMessage($"EXCEPTION: {ex}");
                     Assert.False(true, ex.Message);
                 }
             }
+        }
+
+        private void LogMessage(string message)
+        {
+            Console.WriteLine($"{DateTime.Now} - {GetType()} - {message}");
         }
 
         protected abstract Task ProcessAsync(IKronosClient client);
