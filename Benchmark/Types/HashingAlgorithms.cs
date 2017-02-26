5f44be7651644dcb9254a5bca08868c55e695ee9
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-02-26 19:23:50 +0100

    Added farmhash

--- Benchmark/Types/HashingAlgorithms.cs
+++ Benchmark/Types/HashingAlgorithms.cs
@@ -6,21 +6,30 @@ namespace Benchmark.Types
     public class HashingAlgorithms
     {
         [Params(
-            "abc",
+            "030B4A82-1B7C-11CF-9D53-00AA003C9CB6",
+            "Lorem ipsum",
             "Lorem ipsum dolor sit amet",
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt")]
         public string Word { get; set; }
 
+        private static readonly Murmur3AUnsafe murmur = new Murmur3AUnsafe();
+
         [Benchmark]
-        public int Classic()
+        public uint Murmur3()
         {
-            return Word.GetHashCode();
+            return murmur.Hash(Word);
         }
 
         [Benchmark]
-        public int City()
+        public uint City()
         {
-            return (int)CityHash.CityHash32(Word);
+            return CityHash.CityHash32(Word);
+        }
+
+        [Benchmark]
+        public uint Farm()
+        {
+            return Farmhash.Hash32(Word);
         }
     }
 }
