4f172e2de35082ef5c96a32a47b95cf7d2994840
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-02-27 09:17:23 +0100

    Added basic model for auth

---
+++ Src/Kronos.Core/Configuration/AuthConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace Kronos.Core.Configuration
+{
+    [DataContract]
+    public class AuthConfig
+    {
+        [DataMember]
+        public string Login { get; set; }
+
+        [DataMember]
+        public string Password { get; set; }
+    }
+}
