78ef5baeaf9b33479a82f3cd7e41ae194bbbac68
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-03-17 12:54:29 +0100

    Update runTests.sh

--- Scripts/Linux/runTests.sh
+++ Scripts/Linux/runTests.sh
@@ -4,7 +4,7 @@ set -e
 dotnet restore
 
 RUNTIME="netcoreapp1.1"
-dotnet test Tests/Kronos.Core.Tests/project.json -f $RUNTIME
-dotnet test Tests/Kronos.Client.Tests/project.json -f $RUNTIME
-dotnet test Tests/Kronos.Server.Tests/project.json -f $RUNTIME
-dotnet test Tests/Kronos.AcceptanceTest/project.json -f $RUNTIME
+dotnet test Tests/Kronos.Core.Tests/Kronos.Core.Tests.csproj -f $RUNTIME
+dotnet test Tests/Kronos.Client.Tests/Kronos.Client.Tests.csproj -f $RUNTIME
+dotnet test Tests/Kronos.Server.Tests/Kronos.Server.Tests.csproj -f $RUNTIME
+dotnet test Tests/Kronos.AcceptanceTest/Kronos.AcceptanceTest.csproj -f $RUNTIME
