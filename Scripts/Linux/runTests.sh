8e8d8b6a475e32a6f19690f2f4802688ca54083b
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-03-17 13:22:25 +0100

    Removed support for mono - .NET doesn't support it...

--- Scripts/Linux/runTests.sh
+++ Scripts/Linux/runTests.sh
@@ -3,8 +3,10 @@ set -e
 
 dotnet restore
 
+MODE="Release"
 RUNTIME="netcoreapp1.1"
-dotnet test Tests/Kronos.Core.Tests/Kronos.Core.Tests.csproj -f $RUNTIME
-dotnet test Tests/Kronos.Client.Tests/Kronos.Client.Tests.csproj -f $RUNTIME
-dotnet test Tests/Kronos.Server.Tests/Kronos.Server.Tests.csproj -f $RUNTIME
-dotnet test Tests/Kronos.AcceptanceTest/Kronos.AcceptanceTest.csproj -f $RUNTIME
+
+dotnet test Tests/Kronos.Core.Tests/Kronos.Core.Tests.csproj -c $MODE -f $RUNTIME
+dotnet test Tests/Kronos.Client.Tests/Kronos.Client.Tests.csproj -c $MODE -f $RUNTIME
+dotnet test Tests/Kronos.Server.Tests/Kronos.Server.Tests.csproj -c $MODE -f $RUNTIME
+dotnet test Tests/Kronos.AcceptanceTest/Kronos.AcceptanceTest.csproj -c $MODE -f $RUNTIME
