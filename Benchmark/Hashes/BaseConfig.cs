b5ded6dfcabbd1c574762bdcf81a0e9c6c9b1d04
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-05-09 07:51:25 +0200

    Added hashes to benchmark

---
+++ Benchmark/Hashes/BaseConfig.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Exporters.Csv;
+
+namespace Benchmark.Hashes
+{
+    public class BaseConfig : ManualConfig
+    {
+        public BaseConfig()
+        {
+            Add(StatisticColumn.Mean);
+            Add(StatisticColumn.StdErr);
+            Add(StatisticColumn.StdDev);
+            Add(StatisticColumn.Median);
+            Add(CsvMeasurementsExporter.Default);
+            Add(RPlotExporter.Default);
+            Add(MemoryDiagnoser.Default);
+        }
+    }
+}
