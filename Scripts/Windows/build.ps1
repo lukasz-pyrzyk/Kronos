4055734a21fab6d43071956a02523a98fd29bd93
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2018-07-10 20:28:25 +0200

    Faster builds

--- Scripts/Windows/build.ps1
+++ Scripts/Windows/build.ps1
@@ -1,34 +1,6 @@
-param (
-    [Parameter(Mandatory=$true)][string]$version = 0
-)
-
-# projects to build
-$projects = @(
-    "Src\Kronos.Core",
-    "Src\Kronos.Client",
-    "Src\Kronos.Server"
-)
-
-# build function for project
-function Build($path) {
-    dotnet build $path -c Release --version-suffix $version --no-incremental
-}
-
-function RestorePackages(){
-    dotnet restore
-}
-
 write-host "Build started"
 
-# restore packages
-write-host "Restoring packages"
-RestorePackages
-
-# build each project
-foreach ($project in $projects){
-    write-host "Building " $project "with suffix version " $version
-    Build($project)
-}
+dotnet build
 
 # Set build as failed if any error occurred
 if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
