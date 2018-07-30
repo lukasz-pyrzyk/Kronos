c30fd4e2efd72040eb5043334d12f8fb8a3bc47e
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2018-07-30 19:11:11 +0200

    No more order provider

--- Benchmark/Config/CustomConfig.cs
+++ Benchmark/Config/CustomConfig.cs
@@ -1,14 +1,11 @@
-﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Columns;
+﻿using BenchmarkDotNet.Columns;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Exporters;
 using BenchmarkDotNet.Exporters.Csv;
-using BenchmarkDotNet.Order;
 
 namespace Benchmark.Config
 {
-    [OrderProvider(SummaryOrderPolicy.FastestToSlowest)]
     public class CustomConfig : ManualConfig
     {
         public CustomConfig()
