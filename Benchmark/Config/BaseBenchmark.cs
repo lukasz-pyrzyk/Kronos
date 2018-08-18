b4d48577044ccf021aa4e90ffd306fe3b46e9fa4
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2018-08-18 14:30:36 +0200

    Benchmark with server

--- Benchmark/Config/BaseBenchmark.cs
+++ Benchmark/Config/BaseBenchmark.cs
@@ -1,25 +1,90 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿using System;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
 using Kronos.Client;
 using Kronos.Core.Configuration;
+using Kronos.Server;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
 
 namespace Benchmark.Config
 {
     [Config(typeof(CustomConfig))]
-    public abstract class BaseBenchmark
+    public abstract class BaseBenchmark : IDisposable
     {
         private const string Domain = "127.0.0.1";
         private static readonly string KronosConnection = Domain;
+        private Task _server;
 
         protected IKronosClient KronosClient { get; private set; }
 
         [GlobalSetup]
         public void Setup()
         {
-            KronosClient = KronosClientFactory.FromIp(KronosConnection, Settings.DefaultPort);
+            try
+            {
+                _server = Task.Factory.StartNew(() => Kronos.Server.Program.Start(new SettingsArgs(), GetLoggerConfig()),
+                    TaskCreationOptions.LongRunning);
 
+                while (!Kronos.Server.Program.IsWorking)
+                {
+                    LogMessage("Waiting for server warnup...");
+                    Task.Delay(100).GetAwaiter().GetResult();
+
+                    if (_server.IsFaulted)
+                    {
+                        LogMessage($"Server is faulted. Exception: {_server.Exception}");
+                        throw _server.Exception;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogMessage($"EXCEPTION: {ex}");
+            }
+            KronosClient = KronosClientFactory.FromIp(KronosConnection, Settings.DefaultPort);
             AdditionalSetup();
         }
 
+        private void LogMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
         protected virtual void AdditionalSetup() { }
+
+        private static LoggingConfiguration GetLoggerConfig()
+        {
+            var config = new LoggingConfiguration();
+            config.AddTarget("console", new ConsoleTarget());
+            config.AddRule(LogLevel.Error, LogLevel.Fatal, "console");
+            return config;
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                LogMessage("Stopping server");
+                Kronos.Server.Program.Stop();
+
+                LogMessage("Waiting for server task to finish");
+                _server?.GetAwaiter().GetResult();
+
+                LogMessage("Server stopped");
+            }
+            catch (AggregateException aex)
+            {
+                foreach (var ex in aex.InnerExceptions)
+                {
+                    LogMessage(ex.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                LogMessage($"EXCEPTION: {ex}");
+            }
+        }
     }
 }
