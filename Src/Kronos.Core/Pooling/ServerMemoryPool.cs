fb641e3baf3b7dd38f54b71268f891b409d11e7e
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2018-08-19 09:45:28 +0200

    Struct for rented memory

--- Src/Kronos.Core/Pooling/ServerMemoryPool.cs
+++ Src/Kronos.Core/Pooling/ServerMemoryPool.cs
@@ -4,13 +4,13 @@ using System.Collections.Generic;
 
 namespace Kronos.Core.Pooling
 {
-    public class MemoryItem : IMemoryOwner<byte>
+    public struct RentedMemory : IMemoryOwner<byte>
     {
         private readonly ServerMemoryPool _pool;
         public IMemoryOwner<byte> Owner { get; }
         public Memory<byte> Memory => Owner.Memory;
 
-        public MemoryItem(IMemoryOwner<byte> owner, ServerMemoryPool pool)
+        public RentedMemory(IMemoryOwner<byte> owner, ServerMemoryPool pool)
         {
             Owner = owner;
             _pool = pool;
@@ -25,17 +25,17 @@ namespace Kronos.Core.Pooling
     public class ServerMemoryPool
     {
         private readonly MemoryPool<byte> _pool = MemoryPool<byte>.Shared;
-        private readonly List<MemoryItem> _items = new List<MemoryItem>();
+        private readonly List<RentedMemory> _items = new List<RentedMemory>();
 
         public IMemoryOwner<byte> Rent(int count)
         {
             var owner = _pool.Rent(count);
-            var item = new MemoryItem(owner, this);
+            var item = new RentedMemory(owner, this);
             _items.Add(item);
             return item;
         }
 
-        public void Return(MemoryItem item)
+        public void Return(RentedMemory item)
         {
             item.Owner.Dispose();
             _items.Remove(item);
