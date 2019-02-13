a9d98fddf090290053a13c7e9bb91a33bceb9356
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2019-02-13 21:45:27 +0100

    Version bump

--- Src/Kronos.Client/Connection.cs
+++ Src/Kronos.Client/Connection.cs
@@ -9,52 +9,41 @@ using Kronos.Core.Configuration;
 using Kronos.Core.Exceptions;
 using Kronos.Core.Messages;
 using Kronos.Core.Networking;
-using Polly;
-using BufferedStream = Kronos.Core.Pooling.BufferedStream;
+using Kronos.Core.Pooling;
 
 namespace Kronos.Client
 {
     public class Connection : IConnection
     {
-        private const int RetryCount = 3;
-        private static readonly Policy Policy = Policy.Handle<Exception>()
-            .WaitAndRetryAsync(CreateExponentialBackoff(RetryCount));
-
-        private readonly BufferedStream _stream = new BufferedStream();
         private readonly byte[] _sizeBytes = new byte[sizeof(int)];
+        private readonly BufferedStream _stream = new BufferedStream();
 
         public async Task<Response> SendAsync(Request request, ServerConfig server)
         {
             Socket socket = null;
             Response response = null;
-            await Policy.ExecuteAsync(async () =>
+            try
             {
-                try
-                {
-                    Trace.WriteLine("Connecting to the server socket");
-                    socket = new Socket(SocketType.Stream, ProtocolType.IP);
-                    await socket.ConnectAsync(server.EndPoint).ConfigureAwait(false);
+                Trace.WriteLine("Connecting to the server socket");
+                socket = new Socket(SocketType.Stream, ProtocolType.IP);
+                await socket.ConnectAsync(server.EndPoint).ConfigureAwait(false);
 
-                    Trace.WriteLine("Sending request");
-                    await SendAsync(request, socket).ConfigureAwait(false);
+                Trace.WriteLine("Sending request");
+                await SendAsync(request, socket).ConfigureAwait(false);
 
-                    Trace.WriteLine("Waiting for response");
-                    response = await ReceiveAndDeserializeAsync(socket).ConfigureAwait(false);
-                }
-                catch (Exception ex)
-                {
-                    throw new KronosCommunicationException($"Connection to the {server.EndPoint} has been refused", ex);
-                }
-                finally
-                {
-                    if (!_stream.IsClean)
-                    {
-                        _stream.Clean();
-                    }
+                Trace.WriteLine("Waiting for response");
+                response = await ReceiveAndDeserializeAsync(socket).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                throw new KronosCommunicationException($"Connection to the {server.EndPoint} has been refused", ex);
+            }
+            finally
+            {
+                if (!_stream.IsClean) _stream.Clean();
 
-                    socket?.Dispose();
-                }
-            }).ConfigureAwait(false);
+                socket?.Dispose();
+            }
 
             return response;
         }
@@ -65,7 +54,7 @@ namespace Kronos.Client
 
             try
             {
-                await SocketUtils.SendAllAsync(server, _stream.RawBytes, (int)_stream.Length).ConfigureAwait(false);
+                await SocketUtils.SendAllAsync(server, _stream.RawBytes, (int) _stream.Length).ConfigureAwait(false);
             }
             finally
             {
@@ -76,10 +65,10 @@ namespace Kronos.Client
         private async Task<Response> ReceiveAndDeserializeAsync(Socket socket)
         {
             await SocketUtils.ReceiveAllAsync(socket, _sizeBytes, _sizeBytes.Length).ConfigureAwait(false);
-            int packageSize = BitConverter.ToInt32(_sizeBytes, 0);
+            var packageSize = BitConverter.ToInt32(_sizeBytes, 0);
             Array.Clear(_sizeBytes, 0, _sizeBytes.Length);
 
-            byte[] requestBytes = ArrayPool<byte>.Shared.Rent(packageSize);
+            var requestBytes = ArrayPool<byte>.Shared.Rent(packageSize);
             try
             {
                 await SocketUtils.ReceiveAllAsync(socket, requestBytes, packageSize).ConfigureAwait(false);
@@ -89,17 +78,14 @@ namespace Kronos.Client
                 ArrayPool<byte>.Shared.Return(requestBytes);
             }
 
-            Response response = Response.Parser.ParseFrom(new CodedInputStream(requestBytes, 0, packageSize));
+            var response = Response.Parser.ParseFrom(new CodedInputStream(requestBytes, 0, packageSize));
 
             return response;
         }
 
         private static IEnumerable<TimeSpan> CreateExponentialBackoff(int retryCount)
         {
-            for (int i = 1; i <= retryCount; i++)
-            {
-                yield return TimeSpan.FromSeconds(i * i);
-            }
+            for (var i = 1; i <= retryCount; i++) yield return TimeSpan.FromSeconds(i * i);
         }
     }
 }
