cc43c1da1531703a46c6a540e64c1ebb55c5168f
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2019-11-20 22:23:38 +0100

    ConcurrentPriorityQueue

--- Src/Kronos.Server/Storage/ConcurrentPriorityQueue.cs
+++ Src/Kronos.Server/Storage/ConcurrentPriorityQueue.cs
@@ -15,89 +15,128 @@ namespace Kronos.Server.Storage
             get
             {
                 _lock.EnterReadLock();
-                int count = _nodes.Count;
-                _lock.ExitReadLock();
-                return count;
+                try
+                {
+                    return _nodes.Count;
+                }
+                finally
+                {
+                    _lock.ExitReadLock();
+                }
             }
         }
 
         public void Add(T item)
         {
             _lock.EnterWriteLock();
-            if (_nodes.Count == 0)
+            try
             {
-                _nodes.AddLast(item);
-            }
-            else
-            {
-                var current = _nodes.First;
-
-                while (current != null && current.Value.CompareTo(item) > 0)
-                {
-                    current = current.Next;
-                }
-
-                if (current is null)
+                if (_nodes.Count == 0)
                 {
                     _nodes.AddLast(item);
                 }
                 else
                 {
-                    _nodes.AddBefore(current, item);
+                    var current = _nodes.First;
+
+                    while (current != null && current.Value.CompareTo(item) > 0)
+                    {
+                        current = current.Next;
+                    }
+
+                    if (current is null)
+                    {
+                        _nodes.AddLast(item);
+                    }
+                    else
+                    {
+                        _nodes.AddBefore(current, item);
+                    }
                 }
             }
-            _lock.ExitWriteLock();
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
         }
 
         public T Poll()
         {
             _lock.EnterWriteLock();
-            if (_nodes.Count == 0)
+            try
             {
-                throw new InvalidOperationException("Queue is empty");
+                if (_nodes.Count == 0)
+                {
+                    throw new InvalidOperationException("Queue is empty");
+                }
+
+                T value = _nodes.First.Value;
+                _nodes.RemoveFirst();
+
+                return value;
             }
-
-            T value = _nodes.First.Value;
-            _nodes.RemoveFirst();
-
-            _lock.ExitWriteLock();
-            return value;
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
         }
 
 
         public T Peek()
         {
             _lock.EnterReadLock();
-            if (_nodes.Count == 0)
+            try
             {
-                throw new InvalidOperationException("Queue is empty");
-            }
+                if (_nodes.Count == 0)
+                {
+                    throw new InvalidOperationException("Queue is empty");
+                }
 
-            var item = _nodes.First.Value;
-            _lock.ExitReadLock();
-            return item;
+                return _nodes.First.Value;
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
         }
 
         public void Remove(T item)
         {
             _lock.EnterWriteLock();
-            _nodes.Remove(item);
-            _lock.ExitWriteLock();
+            try
+            {
+                _nodes.Remove(item);
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
         }
 
         public void Clear()
         {
             _lock.EnterWriteLock();
-            _nodes.Clear();
-            _lock.ExitWriteLock();
+            try
+            {
+                _nodes.Clear();
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
         }
 
         public T[] ToArray()
         {
             _lock.EnterReadLock();
-            var array = _nodes.ToArray();
-            _lock.ExitReadLock();
-            return array;
+            try
+            {
+                return _nodes.ToArray();
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
         }
     }
 }
